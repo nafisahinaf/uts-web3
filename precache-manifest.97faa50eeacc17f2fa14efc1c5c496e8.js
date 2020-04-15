352894c8940fc5a7fe9615cb8592dafec3d1fbf9
Nafisah <nafisahkarimah@mail.ugm.ac.id>
2020-04-15 16:10:20 +0700

    Updates

---
+++ precache-manifest.97faa50eeacc17f2fa14efc1c5c496e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770576a0af53e6d89aa37886dfb4649c",
+    "url": "/nafisahinaf/uts-web3/index.html"
+  },
+  {
+    "revision": "748d18d2bc337404709f",
+    "url": "/nafisahinaf/uts-web3/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "df91f8434527589d7ea5",
+    "url": "/nafisahinaf/uts-web3/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "748d18d2bc337404709f",
+    "url": "/nafisahinaf/uts-web3/static/js/2.91f64f4a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nafisahinaf/uts-web3/static/js/2.91f64f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df91f8434527589d7ea5",
+    "url": "/nafisahinaf/uts-web3/static/js/main.04ee2e8f.chunk.js"
+  },
+  {
+    "revision": "455d424531e33d3d71ba",
+    "url": "/nafisahinaf/uts-web3/static/js/runtime-main.665e10a0.js"
+  }
+]);
